a00d06b85074d0c14218680d2845b1ef14ec4c0c
zeinab2309 <poormoosavie0@gmail.com>
2025-05-09 19:15:16 +0330

    add pagination

--- social/views.py
+++ social/views.py
@@ -11,7 +11,7 @@ from taggit.models import Tag
 from django.db.models import Count
 from django.contrib.postgres.search import TrigramSimilarity
 from django.db.models import Q
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 
 
 # Create your views here.
@@ -77,6 +77,14 @@ def post_list(request,tag_slug=None):
     if tag_slug:
         tag=get_object_or_404(Tag,slug=tag_slug)
         posts=Post.objects.filter(tags__in=[tag])
+    paginator=Paginator(posts,3)
+    page_number=request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)  # اگر عدد ای بالا بود و وجود نداشت صفحه لیست اخر را نمایش بده
+    except PageNotAnInteger:
+        posts = paginator.page(1)
     context={
         'posts':posts,
         'tag':tag,
